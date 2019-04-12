bbd818fd7419b77ffb9d384dbbf1bb017ce7f7a3
Marko Lalic <marko.lalic@netcast.rs>
2019-04-12 19:21:18 +0200

    feasibility microservice added

---
+++ feasibility/src/main/java/si/pvr/feasibility/FeasibilityApplication.java
@@ -0,0 +1,13 @@
+package si.pvr.feasibility;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeasibilityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeasibilityApplication.class, args);
+    }
+
+}
