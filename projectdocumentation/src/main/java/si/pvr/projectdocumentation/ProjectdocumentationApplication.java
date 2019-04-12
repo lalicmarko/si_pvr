679e551b362a5bb78c43b09cd2d1c67050f957bc
Marko Lalic <marko.lalic@netcast.rs>
2019-04-12 19:21:44 +0200

    projectdocumentatino microservice added

---
+++ projectdocumentation/src/main/java/si/pvr/projectdocumentation/ProjectdocumentationApplication.java
@@ -0,0 +1,13 @@
+package si.pvr.projectdocumentation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectdocumentationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectdocumentationApplication.class, args);
+    }
+
+}
---
+++ projectdocumentation/src/test/java/si/pvr/projectdocumentation/ProjectdocumentationApplicationTests.java
@@ -0,0 +1,16 @@
+package si.pvr.projectdocumentation;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProjectdocumentationApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
